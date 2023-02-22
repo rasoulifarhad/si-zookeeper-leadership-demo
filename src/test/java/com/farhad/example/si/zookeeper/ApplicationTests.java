fba22c6dbc59c682463471c769ba74f710d5eb1b
Farhad rasouli <rasouli.farhad@gmail.com>
2023-02-22 18:06:31 +0330

    updated

--- src/test/java/com/farhad/example/si/zookeeper/ApplicationTests.java
+++ src/test/java/com/farhad/example/si/zookeeper/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.farhad.example.si.zookeeper.leaderdemo;
+package com.farhad.example.si.zookeeper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SiZookeeperLeaderDemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
